5114fe5466568e97cd250f16a0a103c3e95e72b2
cflores <flores@petalphile.com>
2013-02-20 18:04:49 -0800

    start to support multiple q's, hack-ily

--- bigboard.js
+++ bigboard.js
@@ -18,24 +18,39 @@ if (Meteor.isClient) {
   Template.bigboard.answers = function () {
 //    console.log(Questions.find({}, {answers: {sort: {score: -1, text: 1}}}));
 //    console.log(Questions.find({}, {sort: {score: -1, text: 1}}));
-    return Questions.find({}, {sort: {score: -1, text: 1}});
+//    return Questions.find({question:"What is an indispensible tool?"}, {sort: {'answers.score': -1, 'answers.text': 1}});
+    var pie= Questions.find({question:"What is an indispensible tool?"}, {answers: 1, _id:0});
+    var cake = '';
+    // OMG why
+    pie.forEach(function (answer) {
+      cake = answer.answers;
+    });
+    console.log(cake);
+    return cake;
   };
 
   Template.admin.answers = function () {
 //    console.log(Questions.find({}, {answers: {sort: {score: -1, text: 1}}}));
 //    console.log(Questions.find({}, {sort: {score: -1, text: 1}}));
-    return Questions.find({}, {sort: {score: -1, text: 1}});
+    var pie= Questions.find({question:"What is an indispensible tool?"}, {answers: 1, _id:0});
+    var cake = '';
+    // OMG why
+    pie.forEach(function (answer) {
+      cake = answer.answers;
+    });
+    console.log(cake);
+    return cake;
   };
 
   Template.admin.selected_answer = function () {
 //    var answer = Questions.findOne( {answers: {text: Session.get("selected_answer")}});
-    var answer = Questions.findOne(Session.get("display_answer"));
+    var answer = Questions.findOne({'answers.text':Session.get("display_answer")});
     return answer && answer.text;
   };
 
   Template.bigboard.question_points = function () {
     console.log("i got here");
-    var qscore = Questions.find({display: "yes"}, {score: 1, _id: 0});
+    var qscore = Questions.find({question:"What is an indispensible tool?", 'answers.display': "yes"}, {'answers.score': 1, _id: 0});
     var tscore = 0;
     qscore.forEach (function (each_score) {
       console.log(each_score);
@@ -51,8 +66,8 @@ if (Meteor.isClient) {
 
   Template.admin.events({
     'click input.display': function () {
-      console.log(Questions.update(Session.get("display_answer"), {$set: {display: "yes"}}));
-      Questions.update(Session.get("display_answer"), {$set: {display: "yes"}});
+      console.log(Questions.update(Session.get("display_answer"), {$set: {'answers.display': "yes"}}));
+      Questions.update(Session.get("display_answer"), {$set: {'answers.display': "yes"}});
       console.log("i got here");
     }
   });
@@ -69,49 +84,48 @@ if (Meteor.isServer) {
     console.log("i got here");
     console.log(Questions.find().count());
     if (Questions.find().count() === 0) {
-//      Questions.insert(
-//	{
-//	  question: "What is an indispensible tool?",
-//	  answers:
-	    
-		    Questions.insert({ 
+      Questions.insert(
+	{
+	  question: "What is an indispensible tool?",
+	  answers: [
+		    { 
 		      text: "grep",
 		      score: 50,
 		      display: "no"
-		    });
-		    Questions.insert({
+		    },
+		    {
 		      text: "cat",
 		      score: 10,
 		      display: "no"
-		    });
-		    Questions.insert({
+		    },
+		    {
 		      text: "perl",
 		      score: 20,
 		      display: "no"
-		    });
-		    Questions.insert({
+		    },
+		    {
 		      text: "rm -rf /",
 		      score: 100,
 		      display: "no"
-		    });
-		    Questions.insert({
+		    },
+		    {
 		      text: "nc",
 		      score: 100,
 		      display: "no"
-		    });
-		    Questions.insert({
+		    },
+		    {
 		      text: "cd",
 		      score: 1,
 		      display: "no"
-		    });
-		    Questions.insert({
+		    },
+		    {
 		      text: "echo oh no why |wall",
 		      score: 1000,
 		      display: "no"
-		    });
-//	  ]	    
-//	}
-//      );
+		    }
+	  ]	    
+	}
+      );
 
     }
   });
